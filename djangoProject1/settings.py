5d2bd95fc307cbf45452bd728eaa050c1f775c5f
ChaseThompson8 <74880503+ChaseThompson8@users.noreply.github.com>
2020-11-29 02:38:53 -0500

    static changes

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -123,4 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = ['static']
-STATICFILES_ROOT = '/home/ChaseTSchool/chasetschool.pythonanywhere.com/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
